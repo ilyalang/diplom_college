a037b34f1d2f8d22b3ffc55b87a8df1c01de1f2a
ilyalang <ilyalang95@gmail.com>
2019-01-17 18:46:12 +0300

    path1

---
+++ site/index.php
@@ -0,0 +1,38 @@
+<?php 
+
+
+
+?>
+
+
+<html> 
+	<head>
+			<title>Вопросник</title>
+			<meta lang="ru">
+			<link rel="stylesheet" type="text/css" href="style.css">
+	 </head>
+		<body> 
+			<div class="flex">
+    			<div class="itm itm1">Мои Вопросы</div>
+    			<div class="itm itm2"><a href="create_question">Создать вопрос</a></div>
+    			<div class="itm itm3"><a href="create_test">Создать тест</a></div>
+    			<div class="itm itm4">Экспорт вопросов</div>
+    			<div class="itm itm5">Выйти из системы</div>
+			</div>
+
+					<div class="flex-2">
+  					<div class="flex-itm2">Короткий ответ</div>
+  					<div class="flex-itm2">Единичный выбор</div>
+  					<div class="flex-itm2">Множественный выбор</div>
+  					<div class="flex-itm2">Да/Нет</div>
+  					<div class="flex-itm2">Числовой</div>
+  					<div class="flex-itm2">Сопоставление</div>
+  					<div class="flex-itm2">Упорядочивание</div>
+
+</div>
+
+		</body>
+
+
+
+</html>
